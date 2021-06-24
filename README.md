294cd9969c6a6321994cddaee12a94ced06e24ce
André Pereira <alpes132@hotmail.com>
2021-06-24 16:46:39 -0300

    :tada: Challenge finalized

---
+++ README.md
@@ -0,0 +1,47 @@
+# Ignite ReactJS - Challenge: Creating shopping cart hook
+<br>
+
+#### Status: **Concluded** ✅
+
+## Description
+Implementing a shopping cart hook 
+
+## Functionalities ⭐️
+- Add products in the cart
+- Remove  products in the cart
+- Increment amount product
+- Decrement amount product
+- Subtotal of the item
+- Sum total products in the cart
+
+## Prerequisites
+- Yarn
+
+## Installation
+```
+git clone https://github.com/al-pereira/ignite-reactjs-creating-shopping-cart-hook.git
+
+cd ignite-reactjs-creating-shopping-cart-hook
+
+yarn
+```
+
+## Use
+
+Run the fake API on the terminal :
+```
+yarn server
+```
+
+On another terminal, run the application:
+```
+yarn start
+```
+
+Access the a url
+```
+http://localhost:3333
+```
+
+## Licence
+MIT
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,66 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart];
+      const productExists: Product | undefined = updatedCart.find(product => product.id === productId);
+
+      const currentAmountInCart = productExists ? productExists.amount : 0;
+
+      const desiredAmount = currentAmountInCart + 1;
+
+      const stock = await api.get(`/stock/${productId}`);
+
+      if (desiredAmount > stock.data.amount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      if (productExists) {
+        productExists.amount = desiredAmount;
+      } else {
+        const product = await api.get(`/products/${productId}`);
+
+        const newProduct = {
+          ...product.data,
+          amount: desiredAmount
+        };
+
+        updatedCart.push(newProduct);
+      }
+
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const productExists = cart.find(product => product.id === productId);
+      
+      if (!productExists) {
+        throw Error();
+      }
+      
+      const updatedCart = cart.filter(product => product.id !== productId);
+      
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
@@ -53,9 +91,33 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) {
+        return;
+      }
+      
+      // Check stock
+      const stock = await api.get(`stock/${productId}`);
+
+      if (amount > stock.data.amount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      const updatedCart = [...cart];
+
+      const product = updatedCart.find(product => product.id === productId);
+
+      if (!product) { 
+        throw Error();
+      }
+
+      product.amount = amount;
+
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
